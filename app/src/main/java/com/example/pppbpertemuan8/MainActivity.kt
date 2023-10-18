6b64cee561bf0564a214e263613fcd0076be789e
senaajibayumurti <senaajibayumurti@mail.ugm.ac.id>
2023-10-18 10:12:47 +0700

    Custom Button

--- app/src/main/java/com/example/pppbpertemuan8/MainActivity.kt
+++ app/src/main/java/com/example/pppbpertemuan8/MainActivity.kt
@@ -14,6 +14,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         var binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
