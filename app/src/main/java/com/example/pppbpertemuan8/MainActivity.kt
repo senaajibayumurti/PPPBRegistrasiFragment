49d2e716237157a689e62e57f9265e55699137d0
senaajibayumurti <senaajibayumurti@mail.ugm.ac.id>
2023-10-18 10:13:50 +0700

    Hide Action Bar, judulnya

--- app/src/main/java/com/example/pppbpertemuan8/MainActivity.kt
+++ app/src/main/java/com/example/pppbpertemuan8/MainActivity.kt
@@ -15,6 +15,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        title = "Dashboard"
+        supportActionBar?.hide()
+
         var binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
