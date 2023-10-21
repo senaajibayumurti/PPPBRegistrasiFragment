54374238a923b55e0eedd7c4ec74aae04922ce28
senaajibayumurti <senaajibayumurti@mail.ugm.ac.id>
2023-10-21 21:49:54 +0700

    Kurang tombol exit

--- app/src/main/java/com/example/pppbpertemuan8/WelcomeActivity.kt
+++ app/src/main/java/com/example/pppbpertemuan8/WelcomeActivity.kt
@@ -2,10 +2,13 @@ package com.example.pppbpertemuan8
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.pppbpertemuan8.databinding.ActivityMainBinding
 
 class WelcomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_welcome)
+        var binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        title = "MyTabLayout"
     }
 }
