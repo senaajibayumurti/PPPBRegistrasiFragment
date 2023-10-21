bbffbc7db2b907d0e49ffcbad908cf725efa1b1e
senaajibayumurti <senaajibayumurti@mail.ugm.ac.id>
2023-10-21 22:34:02 +0700

    Finish

--- app/src/main/java/com/example/pppbpertemuan8/WelcomeActivity.kt
+++ app/src/main/java/com/example/pppbpertemuan8/WelcomeActivity.kt
@@ -2,13 +2,30 @@ package com.example.pppbpertemuan8
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import com.example.pppbpertemuan8.databinding.ActivityMainBinding
+import com.example.pppbpertemuan8.databinding.ActivityWelcomeBinding
 
 class WelcomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        var binding = ActivityMainBinding.inflate(layoutInflater)
+        var binding = ActivityWelcomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
         title = "MyTabLayout"
+    }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_option, menu)
+        return true
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId){
+            R.id.action_quit ->{
+                finish()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
